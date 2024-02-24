4e0399ce5243e3eb60bba115240b34d6b3ed5524
Lyonsmasawa <lyonsmasawa@gmail.com>
2024-02-23 22:28:25 -0800

    add crispy

--- ims_project/settings.py
+++ ims_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'crispy_bootstrap4',
     'ims',
 ]
 
@@ -122,3 +124,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
+CRISPY_ALLOWED_TEMPLATE_PACKS = (
+    'bootstrap', 'uni_form', 'bootstrap3', 'bootstrap4')
