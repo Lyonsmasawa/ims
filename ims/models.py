952e9e578befc7becc30451e29b340d5f0e8e887
Lyonsmasawa <lyonsmasawa@gmail.com>
2024-02-23 20:07:34 -0800

    start

---
+++ ims/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Items(models.Model):
+    category = models.CharField(max_length=50, blank=True, null=True)
+    item_name = models.CharField(max_length=50, blank=True, null=True)
+    quantity = models.CharField(max_length=50, blank=True, null=True)
+    receive_quantity = models.CharField(max_length=50, blank=True, null=True)
+    category = models.CharField(max_length=50, blank=True, null=True)
+    category = models.CharField(max_length=50, blank=True, null=True)
+    category = models.CharField(max_length=50, blank=True, null=True)
+    category = models.CharField(max_length=50, blank=True, null=True)
+    category = models.CharField(max_length=50, blank=True, null=True)
+    category = models.CharField(max_length=50, blank=True, null=True)
+    category = models.CharField(max_length=50, blank=True, null=True)
+    category = models.CharField(max_length=50, blank=True, null=True)
